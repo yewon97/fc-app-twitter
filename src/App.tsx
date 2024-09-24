edbf416dc1ebba379880b25d7a662924c8b2c855
yewon97 <dpdnjs9936@gmail.com>
2024-09-24 21:55:30 +0900

    firebase 셋팅, firebase auth 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import Layout from "@/components/Layout";
 import Router from "@/components/Router";
+import { getAuth } from "firebase/auth";
+import { app } from "@/firebaseApp";
+import { useState } from "react";
 
 export default function App() {
+  const auth = getAuth(app);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth.currentUser,
+  );
+
   return (
     <Layout>
       <Router />
