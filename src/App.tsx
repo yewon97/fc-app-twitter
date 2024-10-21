1ae0652507446cc2e983deba7b7a93f451fb5e84
yewon97 <dpdnjs9936@gmail.com>
2024-10-21 23:27:03 +0900

    회원가입 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import Layout from "@/components/Layout";
 import Router from "@/components/Router";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "@/firebaseApp";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function App() {
   const auth = getAuth(app);
+  const [init, setInit] = useState<boolean>(false);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth.currentUser,
   );
 
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setIsAuthenticated(!!user);
+      setInit(true);
+    });
+  }, [auth]);
+
   return (
     <Layout>
       <ToastContainer />
-      <Router isAuthenticated={isAuthenticated} />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : "loading"}
     </Layout>
   );
 }
