f90620c2c207d9d4376017cd9b7fcfd6658b88f2
yewon97 <dpdnjs9936@gmail.com>
2024-10-24 23:02:41 +0900

    toast 속성값 추가, logout 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,12 @@ export default function App() {
 
   return (
     <Layout>
-      <ToastContainer />
+      <ToastContainer
+        theme="dark"
+        autoClose={1000}
+        hideProgressBar
+        newestOnTop
+      />
       {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
     </Layout>
   );
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,10 +4,19 @@ import { BiUserCircle } from "react-icons/bi";
 import { MdLogin, MdLogout } from "react-icons/md";
 import { useContext } from "react";
 import AuthContext from "@/components/context/AuthContext";
+import { getAuth, signOut } from "firebase/auth";
+import { app } from "@/firebaseApp";
+import { toast } from "react-toastify";
 
 export default function MenuList() {
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    const auth = getAuth(app);
+    await signOut(auth);
+    toast.success("로그아웃이 되었습니다.");
+  };
 
   return (
     <div className="footer">
@@ -26,7 +35,7 @@ export default function MenuList() {
             Login
           </button>
         ) : (
-          <button type="button" onClick={() => navigate("/")}>
+          <button type="button" onClick={handleLogout}>
             <MdLogout />
             Logout
           </button>
