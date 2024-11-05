218df9ab4e41c3a2512b582c3b5652bd9fc1ea02
yewon97 <dpdnjs9936@gmail.com>
2024-11-05 20:06:03 +0900

    jsx 수정

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -52,16 +52,14 @@ export default function HomePage() {
       <PostForm />
 
       {/* Tweet posts */}
-      <div>
-        <div className="post">
-          {posts.length > 0 ? (
-            posts?.map((post) => <PostBox key={post?.id} post={post} />)
-          ) : (
-            <div className="post__no-posts">
-              <div className="post__text">게시글이 없습니다.</div>
-            </div>
-          )}
-        </div>
+      <div className="post">
+        {posts.length > 0 ? (
+          posts?.map((post) => <PostBox key={post?.id} post={post} />)
+        ) : (
+          <div className="post__no-posts">
+            <div className="post__text">게시글이 없습니다.</div>
+          </div>
+        )}
       </div>
     </div>
   );
